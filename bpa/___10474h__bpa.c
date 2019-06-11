0e4af746020db63b3c3bf4699ef6009f2abdb92d
urxp <petr.uricar@gmail.com>
2019-06-11 07:19:19 +0200

    macro name colides

--- bpa/___10474h__bpa.c
+++ bpa/___10474h__bpa.c
@@ -1,8 +1,8 @@
-#define __VERSION__ 2
+#define __BPA__ 2
 
 typedef unsigned long	dword;
 
-#if __VERSION__ == 1
+#if __BPA__ == 1
 
 #define __F(name)	name
 #define __V(name) 	name
@@ -15,7 +15,7 @@ dword __F(___10474h)(const char * elm){
 	return __F(___10240h)(&__V(string__MUSICS_BPA), elm);
 }
 
-#elif __VERSION__ == 2
+#elif __BPA__ == 2
 
 extern const char string__MUSICS_BPA[];
 dword ___10240h(const char *, const char *);
@@ -25,7 +25,7 @@ dword ___10474h(const char * elm){
 	return ___10240h(&string__MUSICS_BPA, elm);
 }
 
-#elif __VERSION__ == 3
+#elif __BPA__ == 3
 
 dword ___10240h(const char *, const char *);
 
