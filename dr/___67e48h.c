bee77a4725a18597584000224c50cfe0d5f1f9d8
urxp <petr.uricar@gmail.com>
2019-06-20 01:02:10 +0200

    trackers header

--- dr/___67e48h.c
+++ dr/___67e48h.c
@@ -1,5 +1,9 @@
 #include "x86.h"
 
+typedef enum TrackerType {
+	NONE, SCREAM_TRACKER_3, FAST_TRACKER_2
+} TrackerType;
+
     dword ___5f080h(dword, dword);
     void ___677cch(dword, dword, dword);
     dword ___67bbch(dword, dword, dword, dword);
@@ -21,8 +25,8 @@
     void * loadS3M(dword A, const char * cmf, dword * size, dword * instrumentCount);
     extern void * MSX_Ptr;
     extern void * SFX_Ptr;
-    extern dword MSX_Tracker;
-    extern dword SFX_Tracker;
+    extern TrackerType MSX_Tracker;
+    extern TrackerType SFX_Tracker;
     extern byte SFX_Number;
     void * loadXM(dword A, const char * cmf, dword * size, dword * instrumentCount);
     void freeMemory(dword);
@@ -30,56 +34,58 @@
     extern byte IsSoundEnabled;
 
 // 67e48h
-void ___67e48h__tbd(dword tMSX, const char * fMSX,
-                    dword tSFX, const char * fSFX, int nSFX){
+void ___67e48h__tbd(TrackerType tMSX, const char * fMSX,
+                    TrackerType tSFX, const char * fSFX, int nSFX){
 
-    dword   s0, s1, i0, i1;
+	dword   s0, s1, i0, i1;
 
 	if(!(tSFX&&fSFX)){
-        
-        nSFX = 0;
 
-        if(!(tMSX&&fMSX)) return;
-    }
-    else if(nSFX < 1) nSFX = 1;
+		nSFX = 0;
 
-	MSX_Tracker = (tMSX&&fMSX) ? tMSX : 0;
+		if(!(tMSX&&fMSX)) return;
+	}
+	else if(nSFX < 1) nSFX = 1;
+
+	s0 = s1 = i0 = i1 = 0;
+
+	MSX_Tracker = (tMSX&&fMSX) ? tMSX : NONE;
 
     switch(MSX_Tracker){
-    case 1:
+    case SCREAM_TRACKER_3:
         MSX_Ptr = loadS3M(1, fMSX, &s0, &i0);
         break;
-    case 2:
+    case FAST_TRACKER_2:
         MSX_Ptr = loadXM(1, fMSX, &s0, &i0);
         break;
     default:
-	    MSX_Tracker = s0 = i0 = 0;
+	    MSX_Tracker = NONE;
     }
 
-	SFX_Tracker = (tSFX&&fSFX) ? tSFX : 0;
+	SFX_Tracker = (tSFX&&fSFX) ? tSFX : NONE;
 
     switch(SFX_Tracker){
-    case 1:
+    case SCREAM_TRACKER_3:
         SFX_Ptr = loadS3M(0, fSFX, &s1, &i1);
         break;
-    case 2:
+    case FAST_TRACKER_2:
         SFX_Ptr = loadXM(0, fSFX, &s1, &i1);
         break;
     default:
-	    SFX_Tracker = s1 = i1 = 0;
+	    SFX_Tracker = NONE;
     }
 
 	___677cch(s0 + s1, i0 + i1, 0);
-	
+
     switch(MSX_Tracker){
-    case 1:
+    case SCREAM_TRACKER_3:
         B(___19a27ch) = ___716fch(1, MSX_Ptr, s0) - 1;
         break;
-    case 2:
+    case FAST_TRACKER_2:
         B(___19a27ch) = ___75840h(1, MSX_Ptr, s0) - 1;
         break;
     default:
-        B(___19a27ch) = 0xff;
+        B(___19a27ch) = -1;
     }
 
 	W(___19a27eh) = B(___24e7a4h);
@@ -87,10 +93,10 @@ void ___67e48h__tbd(dword tMSX, const char * fMSX,
 	SFX_Number = nSFX;
 
     switch(SFX_Tracker){
-    case 1:
+    case SCREAM_TRACKER_3:
         ___716fch(0, SFX_Ptr, s1);
         break;
-    case 2:
+    case FAST_TRACKER_2:
         ___75840h(0, SFX_Ptr, s1);
         break;
     default:
