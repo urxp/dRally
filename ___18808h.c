e0880cae91ec78b9c75ffe2ba974ce93ff6ab8a6
Szilárd Biró <col.lawrence@gmail.com>
2023-02-05 14:26:55 +0100

    removed the unnecessary byteswaps from ___18808h_cdecl

--- ___18808h.c
+++ ___18808h.c
@@ -19,30 +19,6 @@ void ___18808h_cdecl(char * A1, const char * A2){
 	fclose(fd);
 	
 	dREncryption_decodeSavedGame(sg);
-	for (int i = 0; i < 20; i++) {
-		racer_t* rc = &sg->racers[i];
-		rc->damage = SDL_SwapLE32(rc->damage);
-		rc->engine = SDL_SwapLE32(rc->engine);
-		rc->tires = SDL_SwapLE32(rc->tires);
-		rc->armor = SDL_SwapLE32(rc->armor);
-		rc->car = SDL_SwapLE32(rc->car);
-		rc->color = SDL_SwapLE32(rc->color);
-		rc->money = SDL_SwapLE32(rc->money);
-		rc->loanshark_type = SDL_SwapLE32(rc->loanshark_type);
-		rc->loanshark_counter = SDL_SwapLE32(rc->loanshark_counter);
-		rc->refund = SDL_SwapLE32(rc->refund);
-		rc->face = SDL_SwapLE32(rc->face);
-		rc->points = SDL_SwapLE32(rc->points);
-		rc->rank = SDL_SwapLE32(rc->rank);
-		rc->wins = SDL_SwapLE32(rc->wins);
-		rc->races = SDL_SwapLE32(rc->races);
-		rc->bonus = SDL_SwapLE32(rc->bonus);
-		rc->income = SDL_SwapLE32(rc->income);
-		rc->mines = SDL_SwapLE32(rc->mines);
-		rc->spikes = SDL_SwapLE32(rc->spikes);
-		rc->rocket_fuel = SDL_SwapLE32(rc->rocket_fuel);
-		rc->sabotage = SDL_SwapLE32(rc->sabotage);
-	}
 
 	n = -1;
 	while(++n < 0xf) A1[n] = sg->name[n];
